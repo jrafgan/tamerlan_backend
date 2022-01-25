b1dc0e2144e61a203de3b6bc31915710ed27aa31
Maksat Duishenaliev <maksatovy@gmail.com>
2022-01-25 17:31:16 +0600

    deploy new app

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const rootPath = __dirname;
 module.exports = {
   rootPath,
   uploadPath: path.join(rootPath, 'public/uploads'),
-    dbUrl: process.env.URI_MONGO || 'mongodb://localhost/tamerlan_project',
+    dbUrl: process.env.URI_MONGO || 'mongodb://sofiya:ivtFq8EVC5pLcQh5@ds027345.mlab.com:27345/tamerlan-01',
     mongoOptions: {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false},
   accessTokenLife: 3600, // время задается в сек
   refreshTokenLife: 36000
