9c0e136830ff85bdfbbecb3ac05da4aee388a643
Maksat Duishenaliev <maksatovy@gmail.com>
2022-01-25 21:23:03 +0600

    mongo db url set to app

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const rootPath = __dirname;
 module.exports = {
   rootPath,
   uploadPath: path.join(rootPath, 'public/uploads'),
-    dbUrl: process.env.dbUrl || 'mongodb://sofiya:ivtFq8EVC5pLcQh5@ds027345.mlab.com:27345/tamerlan-01',
+    dbUrl: process.env.dbUrl || "mongodb+srv://sofiya:ivtFq8EVC5pLcQh5@cluster0.d9xtu.mongodb.net/admin?retryWrites=true&w=majority",
     mongoOptions: {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false},
   accessTokenLife: 3600, // время задается в сек
   refreshTokenLife: 36000
